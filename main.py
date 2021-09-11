9ada99e8095a560b27ae4aae4fb7ac234656a48f
Lucas Ribeiro <53449579+LucasRibeiroRJBR@users.noreply.github.com>
2021-09-11 19:42:58 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import cx_Oracle
+import cx_Oracle, os
 
 try:
     connection = cx_Oracle.connect(
@@ -9,11 +9,24 @@ try:
     )
     print(connection.version)
 
-    c = connection.cursor()
-    rows = c.execute("SELECT * FROM XPTO.COURSE_MATTERS").fetchall()
+    while True:
+        id = input('\nDigite o ID do aluno (0 para sair) -> ')
+        os.system('cls')
 
-    print(rows)
+        if id == 'S':
+            break
+        else:
+            pass
 
-except Exception as ex:
-    print(ex)
+        c = connection.cursor()
+        rows = c.execute(f'SELECT * FROM XPTO.STUDENTS WHERE ID = {id}').fetchall()
+
+        print(f'+{"-"*3}+{"-"*50}+{"-"*80}+')
+        print(f'|{"ID":^3}|{"NOME":^50}|{"E-MAIL":^80}|')
+        print(f'|{"-"*3}|{"-"*50}|{"-"*80}|')
+        print(f'|{rows[0][0]:^3}|{rows[0][1]:^50}|{rows[0][2]:^80}|')
+        print(f'+{"-"*3}+{"-"*50}+{"-"*80}+')
+
+except:
+    pass
 
